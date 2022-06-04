d86cd2230287a91594ca9fafbf63f678fc3da25f
Francois <francoisvongue@gmail.com>
2022-06-04 17:00:13 +0300

    add imap

--- src/imap/main.ts
+++ src/imap/main.ts
@@ -1,3 +1,8 @@
+/*
+  WHY we make snapshot right after we created an empty map 
+  and then we have value inside key (below)?
+  we should not get any value
+*/
 export class IncrementalMap<T = number> {
   private store: Map<string, T>[] = [];
   private currentState: Map<string, T> = new Map();
@@ -21,24 +26,3 @@ export class IncrementalMap<T = number> {
     return this.currentState.get(key)!;
   }
 }
-
-const map = new IncrementalMap();
-
-/*
-  WHY we make snapshot right after we created an empty map 
-  and then we have value inside key (below)?
-  we should not get any value
-*/
-
-map.snapshot(0);
-
-map.set('key', 10);
-// map.get('key') === 10
-
-map.snapshot(1);
-
-map.set('key', 20);
-// map.get('key') === 20
-
-map.snapshot(0);
-// map.get('key') === 10 
